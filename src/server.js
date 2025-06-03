266223281e5e23cb80b4583aacf3118a9a66b4d1
Wirayuda <yudaardi01@gmail.com>
2025-06-03 15:21:19 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,50 @@
+const Hapi = require('@hapi/hapi');
+// const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesServices');
+const NotesValidator = require('./validator/notes/index');
+const ClientError = require('./exceptions/ClientError');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 3000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
