04ec8bf8a5f07a1408121128f6cf9f43046dc6f8
Wirayuda <yudaardi01@gmail.com>
2025-06-18 15:15:30 +0700

    add foreign key to notes owner column

---
+++ migrations/1750234018889_add-foreign-key-to-owner-column.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  // memberikan constraint foreign key pada owner terhadap kolom id dari tabel users
+  pgm.addConstraint('notes', 'fk_notes.owner_user.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  // menghapus constraint fk_notes.owner_users.id pada tabel notes
+  pgm.dropConstraint('notes', 'fk_notes.owner_users.id');
+};
